e80287911bb6e8e29e98c411a5aa620ad06e7061
Ksenya <lapkina.ksenya@gmail.com>
2025-10-09 14:51:11 +0300

    Исправлено 1 задание

--- tasks/00_distance.py
+++ tasks/00_distance.py
@@ -1,27 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# Есть словарь координат городов
-sites = {
-    'Moscow': (550, 370),
-    'London': (510, 510),
-    'Paris': (480, 480),
-}
-
-# Составим словарь словарей расстояний между ними
-distances = {}
-
-for city1, coords1 in sites.items():
-    distances[city1] = {}
-    for city2, coords2 in sites.items():
-        if city1 != city2:
-            x1, y1 = coords1
-            x2, y2 = coords2
-            distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-            distances[city1][city2] = round(distance, 2)
-
-print(distances)
-
-
-
-
+def calculate_distances(cities_coords):
+    distances = {}
+    for city1, coords1 in cities_coords.items():
+        distances[city1] = {}
+        for city2, coords2 in cities_coords.items():
+            if city1 != city2:
+                x1, y1 = coords1
+                x2, y2 = coords2
+                distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+                distances[city1][city2] = round(distance, 2)
+    return distances
